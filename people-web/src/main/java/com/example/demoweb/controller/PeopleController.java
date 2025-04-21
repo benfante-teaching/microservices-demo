2d3aa8bfd3361dd6878f8c9c6c274614d0abb737
Lucio Benfante <lucio@benfante.com>
2025-04-21 18:06:07 +0200

    Refactor web application structure: swap Thymeleaf and web starter dependencies, add Bootstrap and Thymeleaf layout dialect, create base and layout templates, and enhance People list page with improved UI and search functionality.

--- people-web/src/main/java/com/example/demoweb/controller/PeopleController.java
+++ people-web/src/main/java/com/example/demoweb/controller/PeopleController.java
@@ -8,13 +8,16 @@ import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.util.UriComponentsBuilder;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 
 @SuppressWarnings("unused")
 @Controller
+@RequestMapping("/people")
 public class PeopleController {
 
     @Autowired
@@ -27,7 +30,7 @@ public class PeopleController {
     private CircuitBreakerFactory<?,?> circuitBreakerFactory;
     
     @GetMapping
-    public String list(Model model) {
+    public String list(@RequestParam(name = "query", defaultValue = "") String query, Model model) {
         CircuitBreaker cb = circuitBreakerFactory.create("peopleServiceCB");
 
         Person[] people = 
@@ -39,7 +42,9 @@ public class PeopleController {
                 // return restTemplate.getForObject(peopleServiceUrl+"/api/v1/people", Person[].class);
 
                 /* *** Comment this block if you want to directly use Eureka client to get the service URL *** */
-                return restTemplate.getForObject("http://DEMOPERSON/api/v1/people", Person[].class);
+                UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://DEMOPERSON/api/v1/people")
+                    .queryParam("q", query);
+                return restTemplate.getForObject(builder.toUriString(), Person[].class);
             },
             (throwable) -> {
                 model.addAttribute("errorMessage", "Il servizio People Service non è disponibile");
